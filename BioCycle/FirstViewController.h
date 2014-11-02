efb0110bb106732fba92b5e0d76cdaab1373379f
Vasiliy Makarov <drmoriarty.0@gmail.com>
2014-11-02 20:12:28 +0300

    calc bio cycle

--- BioCycle/FirstViewController.h
+++ BioCycle/FirstViewController.h
@@ -10,6 +10,12 @@
 
 @interface FirstViewController : UIViewController
 
+@property (nonatomic, strong) IBOutlet UIProgressView *physics;
+@property (nonatomic, strong) IBOutlet UIProgressView *emotions;
+@property (nonatomic, strong) IBOutlet UIProgressView *intelligence;
+@property (nonatomic, strong) IBOutlet UIProgressView *mystical;
+@property (nonatomic, strong) IBOutlet UIDatePicker *datePicker;
+@property (nonatomic, strong) IBOutlet UILabel *today;
 
 @end
 
--- BioCycle/FirstViewController.m
+++ BioCycle/FirstViewController.m
@@ -14,14 +14,80 @@
 
 @implementation FirstViewController
 
+@synthesize physics, emotions, intelligence, mystical, datePicker, today;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSDate *dd = [[NSUserDefaults standardUserDefaults] objectForKey:@"defaultDate"];
+    if(dd) {
+        [datePicker setDate:dd];
+        [self processDate:dd];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(IBAction)selectDate:(UIDatePicker*)sender
+{
+    [self processDate:sender.date];
+    [[NSUserDefaults standardUserDefaults] setObject:sender.date forKey:@"defaultDate"];
+}
+
+-(void)processDate:(NSDate*)date
+{
+    NSDate *now = [NSDate date];
+    today.text = [now description];
+    NSTimeInterval interval = [now timeIntervalSinceDate:date];
+    const CGFloat PhysInt = 22 * 24*60*60;
+    const CGFloat EmotInt = 27 * 24*60*60;
+    const CGFloat IntlInt = 32 * 24*60*60;
+    const CGFloat MystInt = 37 * 24*60*60;
+    CGFloat ph = (interval - (int)(interval/PhysInt)*PhysInt) / PhysInt;
+    CGFloat em = (interval - (int)(interval/EmotInt)*EmotInt) / EmotInt;
+    CGFloat it = (interval - (int)(interval/IntlInt)*IntlInt) / IntlInt;
+    CGFloat ms = (interval - (int)(interval/MystInt)*MystInt) / MystInt;
+    ph = sin(ph * M_PI * 2.f);
+    em = sin(em * M_PI * 2.f);
+    it = sin(it * M_PI * 2.f);
+    ms = sin(ms * M_PI * 2.f);
+    
+    NSLog(@"Physical state: %f", ph);
+    NSLog(@"Emotional state: %f", em);
+    NSLog(@"Intelligent state: %f", it);
+    NSLog(@"Mystical state: %f", ms);
+    
+    if(ph > 0) {
+        [physics setTintColor:[UIColor greenColor]];
+        [physics setProgress:ph];
+    } else {
+        [physics setTintColor:[UIColor redColor]];
+        [physics setProgress:-ph];
+    }
+    if(em > 0) {
+        [emotions setTintColor:[UIColor greenColor]];
+        [emotions setProgress:em];
+    } else {
+        [emotions setTintColor:[UIColor redColor]];
+        [emotions setProgress:-em];
+    }
+    if(it > 0) {
+        [intelligence setTintColor:[UIColor greenColor]];
+        [intelligence setProgress:it];
+    } else {
+        [intelligence setTintColor:[UIColor redColor]];
+        [intelligence setProgress:-it];
+    }
+    if(ms > 0) {
+        [mystical setTintColor:[UIColor greenColor]];
+        [mystical setProgress:ms];
+    } else {
+        [mystical setTintColor:[UIColor redColor]];
+        [mystical setProgress:-ms];
+    }
 }
 
 @end
